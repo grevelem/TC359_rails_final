55573e0578daba111297dd8e7e93da4f011742f9
Emily Grevel <grevelem@msu.edu>
2015-03-29 23:52:55 -0400

    change age restrictions

--- app/models/basketball.rb
+++ app/models/basketball.rb
@@ -1,4 +1,4 @@
 class Basketball < ActiveRecord::Base
   validates_presence_of :name, :age, :position, :college
-  validates :age, length: { is: 2 }
+  validates_numericality_of :age, less_than_or_equal_to: 24, greater_than: 17
 end
